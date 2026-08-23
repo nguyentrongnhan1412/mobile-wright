77f0c30237ba78376787e96477116186a57d5e2b
Nhan Nguyen Trong <nhan.nguyentrong@nashtechglobal.com>
2026-08-24 03:31:34 +0700

    fix config

--- mobilewright.config.ts
+++ mobilewright.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'mobilewright';
 
 export default defineConfig({
   testDir: './tests',
+  testMatch: '**/*.spec.ts',
   reporter: [['html'], ['allure-playwright', { resultsDir: 'allure-results' }]],
   platform: 'android',
   bundleId: 'com.saucelabs.mydemoapp.android',
